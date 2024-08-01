e5c7b1c686ed457e9e3fa60dbc790e54530e8c31
RACHEL M. HOUTMAN <Rachel.Houtman@usda.gov>
2024-08-01 10:20:52 -0700

    Create 03a_assemble_x_tables.R
    Adding the x table compilation script. Will require rewiring of all hard-coded pathways. Resulting tables are all x tables by zone, excluding the precise coordinate data.

---
+++ gtac_production_scripts/02_Reference_Data_Prep/03a_assemble_x_tables.R
@@ -0,0 +1,57 @@
+library(data.table)
+library(dplyr)
+
+options("scipen"=9999)
+## Combine X table variables into the x table:
+
+biophysical <- as.data.table(read.csv("F:/TreeMap2020/XTable/TM_2020_X_table_Plots_with_biophys_extracted.csv"))
+vegetation <- as.data.table(read.csv("F:/TreeMap2020/XTable/FIA2020_FVS_cover_and_height.csv"))
+evg_zone <- as.data.table(read.csv("F:/TreeMap2020/XTable/LF2022_EVT_GP_byZone_Workaround2_Final.csv"))
+disturbance_data <- as.data.table(read.csv("F:/TreeMap2020/XTable/FIA2020_Disturbance.csv"))
+disturbance_data$PLT_CN <- as.numeric(disturbance_data$PLT_CN)
+elevation <- as.data.table(read.csv("F:/TreeMap2020/XTable/LF_Plots_Elevation.csv"))
+aspect <- as.data.table(read.csv("F:/TreeMap2020/XTable/LF_Plots_Aspect.csv"))
+slope <- as.data.table(read.csv("F:/TreeMap2020/XTable/LF_Plots_Slope.csv"))
+
+elevation$ELEV <- elevation$ELEV * 3.28084
+
+topography <- left_join(elevation[,2:3], aspect[,2:3]) %>% 
+  left_join(slope[,2:3])
+
+topography <- topography[ASPECT == -1, ':='(ASPECT = 0, SLOPE = 0),]
+topography <- topography[PLOT_CN %in% unique(disturbance_data$PLT_CN),]
+# In order to address missing topographic data those plots with slope and aspect missing were
+# filled in by using the intersect of the original FIA plots and the LANDFIRE layers.
+topography_filled_in <- left_join(topography, disturbance_data[,c("PLT_CN", "SLOPE", "ASPECT", "ELEV")], by = c("PLOT_CN" = "PLT_CN"))
+topography_filled_in <- topography_filled_in %>% 
+  mutate(SLOPE = case_when(is.na(SLOPE.y) ~ SLOPE.x,
+                           !is.na(SLOPE.y) ~ SLOPE.y),
+         ASPECT = case_when(is.na(ASPECT.y)  ~ ASPECT.x,
+                            !is.na(ASPECT.y) ~ ASPECT.y)) %>%
+  select(-c(ASPECT.x, ASPECT.y, SLOPE.x, SLOPE.y, ELEV.y)) %>%
+  rename(ELEV = ELEV.x)
+
+
+x_table <- left_join(evg_zone[,c("PLT_CN", "Zone", "EVT_GP"),], 
+                     disturbance_data[,c("PLT_CN", "disturb_code", "disturb_year"),], by = "PLT_CN" ) %>%
+  left_join(biophysical[,c("PLT_CN", "prcp", "srad", "swe", "tmax", "tmin", "vp", "vpd"),]) %>%
+  left_join(vegetation[,c("StandID", "canopy_cover", "canopy_height"),], by = c("PLT_CN" = "StandID")) %>%
+  left_join(topography_filled_in, by = c("PLT_CN" = "PLOT_CN")) 
+
+x_table_complete <- na.omit(x_table)
+
+# Add in the unique TreeMap ID for each CN
+x_table_complete <- as.data.table(x_table_complete %>% 
+  arrange(PLT_CN) %>%
+  group_by(PLT_CN) %>%
+  mutate(TM_ID = cur_group_id()) %>%
+  ungroup())
+  
+x_table_complete <- setcolorder(x_table_complete, c("TM_ID", "Zone", "PLT_CN", "SLOPE", "ASPECT", "ELEV", "prcp", "srad", "swe", 
+                                  "tmax", "tmin", "vp", "vpd", "disturb_code", "disturb_year", "canopy_cover",
+                                  "canopy_height", "EVT_GP"))
+
+for(x in unique(x_table_complete$Zone)){
+  x_table_zone <- x_table_complete[Zone == x,]
+  write.csv(x_table_zone, paste0("F:/TreeMap2020/XTable/X_tables_by_zone/x_table_", x, ".csv"), row.names = FALSE)
+}
