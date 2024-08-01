7839dc17780db40ac13339da3378b4d703adc293
RACHEL M. HOUTMAN <Rachel.Houtman@usda.gov>
2024-08-01 12:13:15 -0700

    Notating x table assembly.
    Adding clarifying notes to x table assembly.

--- gtac_production_scripts/02_Reference_Data_Prep/03a_assemble_x_tables.R
+++ gtac_production_scripts/02_Reference_Data_Prep/03a_assemble_x_tables.R
@@ -13,11 +13,16 @@ elevation <- as.data.table(read.csv("F:/TreeMap2020/XTable/LF_Plots_Elevation.cs
 aspect <- as.data.table(read.csv("F:/TreeMap2020/XTable/LF_Plots_Aspect.csv"))
 slope <- as.data.table(read.csv("F:/TreeMap2020/XTable/LF_Plots_Slope.csv"))
 
+# Convert DEM elevation from meters to feet
 elevation$ELEV <- elevation$ELEV * 3.28084
 
+# Create topography table
 topography <- left_join(elevation[,2:3], aspect[,2:3]) %>% 
   left_join(slope[,2:3])
 
+# Create the final topography table. This selects the topographical values from FIA
+# data first, then fills in missing data with the DEM-plot overlay data. We implicitely 
+# assume FIA plot data is more accurate than the DEM data.
 topography <- topography[ASPECT == -1, ':='(ASPECT = 0, SLOPE = 0),]
 topography <- topography[PLOT_CN %in% unique(disturbance_data$PLT_CN),]
 # In order to address missing topographic data those plots with slope and aspect missing were
@@ -31,13 +36,15 @@ topography_filled_in <- topography_filled_in %>%
   select(-c(ASPECT.x, ASPECT.y, SLOPE.x, SLOPE.y, ELEV.y)) %>%
   rename(ELEV = ELEV.x)
 
-
+# Join all fields for the x table EXCLUDING the coordinate fields. These must be
+# added later as they are CUI. 
 x_table <- left_join(evg_zone[,c("PLT_CN", "Zone", "EVT_GP"),], 
                      disturbance_data[,c("PLT_CN", "disturb_code", "disturb_year"),], by = "PLT_CN" ) %>%
   left_join(biophysical[,c("PLT_CN", "prcp", "srad", "swe", "tmax", "tmin", "vp", "vpd"),]) %>%
   left_join(vegetation[,c("StandID", "canopy_cover", "canopy_height"),], by = c("PLT_CN" = "StandID")) %>%
   left_join(topography_filled_in, by = c("PLT_CN" = "PLOT_CN")) 
 
+# This excludes plots that are missing values for disturbance years. 
 x_table_complete <- na.omit(x_table)
 
 # Add in the unique TreeMap ID for each CN
@@ -46,11 +53,13 @@ x_table_complete <- as.data.table(x_table_complete %>%
   group_by(PLT_CN) %>%
   mutate(TM_ID = cur_group_id()) %>%
   ungroup())
-  
+
+# Order the columns similarly to previous x tables.
 x_table_complete <- setcolorder(x_table_complete, c("TM_ID", "Zone", "PLT_CN", "SLOPE", "ASPECT", "ELEV", "prcp", "srad", "swe", 
                                   "tmax", "tmin", "vp", "vpd", "disturb_code", "disturb_year", "canopy_cover",
                                   "canopy_height", "EVT_GP"))
 
+# write out x tables by zone.
 for(x in unique(x_table_complete$Zone)){
   x_table_zone <- x_table_complete[Zone == x,]
   write.csv(x_table_zone, paste0("F:/TreeMap2020/XTable/X_tables_by_zone/x_table_", x, ".csv"), row.names = FALSE)
