45cde74eeafdc32bdaa67a093aae1c244ec8f243
SCOTT N. ZIMMER <Scott.Zimmer@usda.gov>
2025-01-23 14:05:27 -0700

    Move raster attribute table scripts to new folder

--- gtac_production_scripts/07_Assembly/01_build_imputation_raster_attribute_tables.R
+++ gtac_production_scripts/07_Assembly/01_build_imputation_raster_attribute_tables.R
@@ -3,8 +3,8 @@ library(foreign)
 
 home_dir<- "//166.2.126.25/TreeMap/"
 
-year <- 2020
-project_name <- glue::glue("{year}_Production")
+year <- 2022
+project_name <- glue::glue("{year}_Production_newXtable")
 
 #set path to assembled rasters
 assembled_dir <- glue::glue("{home_dir}03_Outputs/07_Projects/{project_name}/02_Assembled_model_outputs/")
