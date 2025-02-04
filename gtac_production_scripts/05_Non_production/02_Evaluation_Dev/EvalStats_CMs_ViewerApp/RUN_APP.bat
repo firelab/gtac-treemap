541643a593cc47ddf0879de651d1670c08a88784
ABHINAV SHRESTHA <Abhinav.Shrestha@usda.gov>
2025-02-04 15:26:16 -0500

    feature(eval app)/add exception handling to .bat
    * Add `--- SET PARAMETER HERE ---` section in the batch file used to start the eval app ("RUN_APP.bat"). This section directs users to update the path to Rscript based on the location of the Rscript.exe in their machine.
    * Add IF and ELSE code blocks in the batch file ("RUN_APP.bat") to check if the Rscript path exists, warn user if the path does not exist, and launch app if it does.

--- gtac_production_scripts/05_Non_production/02_Evaluation_Dev/EvalStats_CMs_ViewerApp/RUN_APP.bat
+++ gtac_production_scripts/05_Non_production/02_Evaluation_Dev/EvalStats_CMs_ViewerApp/RUN_APP.bat
@@ -3,6 +3,20 @@ REM .bat file statement to keep cmd open even if an error is encountered
 REM https://stackoverflow.com/a/33929280
 IF NOT DEFINED in_subprocess (cmd /k SET in_subprocess=y ^& %0 %*) & EXIT )
 
-REM Connect to Rscript executable located in machin to run R script
-@echo LAUNCHING EVAL APP...
-CALL "C:\Program Files\R\R-4.4.1\bin\Rscript.exe" "run.R"
+REM --- SET PARAMETER HERE ---
+SET R_PATH="C:\Program Files\R\R-4.4.1\bin\Rscript.exe"
+
+REM if R_PATH does not exist, warn user
+IF EXIST %R_PATH% (
+	
+	REM Connect to Rscript executable located in machin to run R script
+	@echo LAUNCHING EVAL APP...
+	CALL %R_PATH% "run.R"
+	
+) ELSE (
+
+	@echo ------------------------------------------------------
+	@echo WARNING: PLEASE OPEN THE .BAT FILE AND SET THE R PATH!
+	@echo ------------------------------------------------------
+	
+)
