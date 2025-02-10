978a7e9a97173d4dc57366d41bc48abd2b067ec6
Nicholas-Storey <nicholas.storey@usda.gov>
2025-02-10 14:35:10 -0700

    updated palette and minor bug fixes

--- gee_viz_setup_scripts/find_missing_codes.py
+++ gee_viz_setup_scripts/find_missing_codes.py
@@ -1,6 +1,6 @@
 import pdfplumber, json, random
 
-outputJson = r'C:\Users\NicholasStorey\gtac-treemap\gee_viz_setup_scripts\forest_type_palette_lookup_UPDATED.json'
+outputJson = r'C:\Users\NicholasStorey\gtac-treemap\gee_viz_setup_scripts\forest_type_palette_lookup_UPDATED_GROUPS.json'
 input_pdf = r"C:\Users\NicholasStorey\Downloads\forestcodes.pdf"
 
 with pdfplumber.open(input_pdf) as pdf:
@@ -10,12 +10,10 @@ newLookup = { 'code': [], 'names': [] }
 for table in tables:
     del table[0]
     for row in table:
-        if 'group' in row[3].lower():
-            continue
         newLookup['code'].append(int(row[2]))
         newLookup['names'].append(row[3])
 
-with open(r"C:\Users\NicholasStorey\gtac-treemap\gee_viz_setup_scripts\forest_type_palette_lookup.json", 'r') as file:
+with open(r"C:\Users\NicholasStorey\gtac-treemap\gee_viz_setup_scripts\forest_type_palette_lookup_UPDATED.json", 'r') as file:
     ogLookup = json.load(file)
     in_new_not_og = sorted(set(newLookup['code']) - set(ogLookup['code']))
 
