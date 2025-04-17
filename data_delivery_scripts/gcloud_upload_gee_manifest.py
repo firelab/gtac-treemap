f04ff42af581fdd7fe1e357d1213a382bba0046a
Nicholas-Storey <nicholas.storey@usda.gov>
2025-04-17 12:19:07 -0600

    gcloud upload script working, updated separate_attributes script to automatically handle DBF character limit

--- data_delivery_scripts/gcloud_upload_gee_manifest.py
+++ data_delivery_scripts/gcloud_upload_gee_manifest.py
@@ -1,32 +1,44 @@
 '''
-This script is a work-in-progress.
+Uploads TreeMap attributes to gcloud and manifests them as bands of a single Earth Engine image stored in an image collection.
 
-Go back to Nick Storey for support on getting multiple images into a single image with multiple bands.
+Run for each image you'd like in the image collection (e.g., once for CONUS image, once for ALASKA image).
 
-It takes all tifs in the format 'TreeMap{year}_*.tif' and uses the assetManagerLib.py from geeViz 
-to upload to gcloud and manifest an asset into earth engine.
+YOU MUST SET THE START AND END DATE OF THE IMAGE COLLECTION IN EARTH ENGINE MANUALLY (THESE CAN BE SET IN THE SCRIPT FOR THE INDIVIDUAL IMAGES)
 '''
-#%%
 
-import os
-import geeViz
-from glob import glob
+import os, ee
 from geeViz import assetManagerLib as aml
+from glob import glob
+from osgeo import gdal
+
+
+#################################################
+# User Variables (edit these)
+#################################################
+
+# Year of the dataset and landfire version used
 year = '2022'
-tm_ver = '2022'
 landfire_ver = '2.0.0'
-image_folder = r'\\166.2.126.25\TreeMap\03_Outputs\04_Separated_Attribute_Rasters\2022'
-gcs_bucket = 'separated-attributes/2022/'
-gee_asset_path = 'treemap-386222/assets/Final_Outputs/2022/TreeMap_CONUS_2022_SeparatedAttributes'
 
-# Get all the images
-images = glob(os.path.join(image_folder, f'TreeMap_CONUS_{tm_ver}_*.tif'))
+# The folder in which to look for images, and the format of the file names to select.
+image_folder = r'\\166.2.126.25\TreeMap\08_Data_Delivery\01_Separated_Attribute_Rasters\2022'
+name_format = f'TreeMap{year}_CONUS_*.tif'
 
-#%%
+# Google Cloud bucket to upload the attributes to and the project id
+gcs_bucket = f'gs://separated-attributes/{year}'
+project_id = 'treemap-386222'
 
-# Band names
-band_names_pyramids = [
-    'FORTYPCD':'MODE',
+# Earth Engine paths
+    # The folder to save the image collection in
+gee_folder = f'projects/treemap-386222/assets/Final_Outputs/{year}'
+    # The name of the image collection to create
+gee_image_collection_name = f'TreeMap{year}'
+    # The name of the image to create
+gee_image_name = f'TreeMap{year}_CONUS'
+
+# pyramidPolicy lookup for attributes
+pyramidPolicy_lookup = {
+    'FORTYPCD': 'MODE',
     'FLDTYPCD':'MODE',
     'STDSZCD':'MODE',
     'FLDSZCD':'MODE',
@@ -47,22 +59,61 @@ band_names_pyramids = [
     'CARBON_L':'MEAN',
     'CARBON_D':'MEAN',
     'CARBON_DOWN_DEAD':'MEAN'
-]
-
-properties = {
-    'studyArea': 'CONUS',
-    'year': year,
-    'landfire_ver':landfire_ver
 }
 
-# Take many images and combine into one via geeViz 
-aml.uploadToGEEImageAsset(
-    images,
+# Properties for the image collection
+image_collection_properties = {
+    'year': year
+}
+
+# Properties for the image
+    # time_start and time_end should be a span of one year (e.g., 2022-01-01, 2023-01-01)
+    # Properties can be changed and new ones can be added manually in Earth Engine if needed
+image_properties = {
+    'studyArea': 'CONUS',
+    'year': year,
+    'landfire_ver': landfire_ver,
+    'system:time_start': ee.Date(f'{year}-01-01'),
+    'system:time_end': ee.Date(f'{int(year)+1}-01-01')
+}
+
+
+#################################################
+# Script Setup
+#################################################
+
+# Initialize Google Cloud and Earth Engine with the project
+ee.Authenticate()
+ee.Initialize(project=project_id)
+
+# For each image in the directory that matches the name format, add it to a dictionary and save its pyramiding policy, band name, and no data value.
+image_dict = {}
+for image in glob(os.path.join(image_folder, name_format)):
+    image_dict[image] = {}
+    # Check for the resample type
+    for attribute in pyramidPolicy_lookup.keys():
+        if attribute in os.path.basename(image):
+            image_dict[image]['pyramidingPolicy'] = pyramidPolicy_lookup[attribute]
+            image_dict[image]['bandName'] = attribute
+
+    # Get the noDataValue via gdal
+    ds = gdal.Open(image)
+    band = ds.GetRasterBand(1)
+    no_data_val = band.GetNoDataValue()
+    image_dict[image]['noDataValue'] = no_data_val
+
+#################################################
+# Main
+#################################################
+
+# Create the image collection
+aml.create_image_collection(f'{gee_folder}/{gee_image_collection_name}', image_collection_properties)
+
+# Create the image (single image, multiple bands of imagery)
+aml.uploadToGEEAssetImagesAsBands(
+    image_dict,
     gcs_bucket,
-    gee_asset_path,
+    f'{gee_folder}/{gee_image_collection_name}/{gee_image_name}',
     overwrite=False,
-    bandNames=list(band_names_pyramids.keys()),
-    properties=properties,
-    pyramidingPolicy=list(band_names_pyramids.values()),
-    noDataValues=None,
+    properties=image_properties
     )
