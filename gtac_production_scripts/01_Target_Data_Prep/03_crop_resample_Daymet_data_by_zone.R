3801e026ea4f2246bc969c9976d496dd4c32c6e0
Lila Leatherman <31491157+lleather@users.noreply.github.com>
2025-05-06 12:34:30 -0600

    remove unused portions of code-- updated to loop over clim vars and take project and zonal input functions

--- gtac_production_scripts/01_Target_Data_Prep/03_crop_resample_Daymet_data_by_zone.R
+++ gtac_production_scripts/01_Target_Data_Prep/03_crop_resample_Daymet_data_by_zone.R
@@ -1,22 +1,19 @@
-# Crop, resample, and initial mask for DayMet climate data
+# Crop, resample, and mask for DayMet climate data
 
 # Written by Scott Zimmer (scott.zimmer@usda.gov)
 # Updated by Lila Leatherman (lila.Leatherman@usda.gov)
 
+# Required inputs: 
+# - Raw climate layers
+# - Reclassed final EVT_GP layers for each zone
 
-# Last Updated: 4/28/25
-
-# Final Output Rasters: 
-
-
-# Intermediate Output Rasters: 
+# Last Updated: 5/6/25
 
 
 
 #################################################################
 # Set Inputs
 #################################################################
-
 # define year
 year_input <- 2023
 
@@ -52,31 +49,13 @@ lf_zone_nums<- sort(lf_zones$ZONE_NUM)
 z = which(lf_zone_num_start==lf_zone_nums)[1] # get index of starting zone
 lf_zone_nums <- lf_zone_nums[z:length(lf_zone_nums)] # list zones to run
 
-
-# Load a landfire raster as a template to match
-lf_rast <- terra::rast(evc_path)
-
-
 # Identify path to Daymet rasters
 daymet_dir <- glue::glue('{home_dir}/01_Data/07_Daymet/daymet_north_america_normal/')
 
 # list climate vars
 clim_vars <- c("prcp", "srad", "swe", "tmax", "tmin", "vp", "vpd")
 
-# prcp<- terra::rast(glue::glue("{daymet_dir}/prcp_normal_1981to2010.tif"))
-# srad<- terra::rast(glue::glue("{daymet_dir}/srad_normal_1981to2010.tif"))
-# swe<- terra::rast(glue::glue("{daymet_dir}/swe_normal_1981to2010.tif"))
-# tmax<- terra::rast(glue::glue("{daymet_dir}/tmax_normal_1981to2010.tif"))
-# tmin<- terra::rast(glue::glue("{daymet_dir}/tmin_normal_1981to2010.tif"))
-# vp<- terra::rast(glue::glue("{daymet_dir}/vp_normal_1981to2010.tif"))
-# vpd<- terra::rast(glue::glue("{daymet_dir}/vpd_normal_1981to2010.tif"))
-
-# 
-
 # Loop through landfire zones, masking, reprojecting, resampling, and saving data by zone ----
-
-# First create output directory 
-#dir.create("./03_Outputs/05_Target_Rasters/v{year}/pre_mask/")
 
 
 for (i in lf_zone_nums){
@@ -90,26 +69,6 @@ for (i in lf_zone_nums){
   targetDataZonalInputs(zone_input)
   
   lf_zone<- lf_zones[lf_zones$ZONE_NUM == zone_input,]
-  
-  # # Make a directory for saving landfire data for the zone
-  # ifelse(i<10, 
-  #        dir.create(paste0("./03_Outputs/05_Target_Rasters/v2023/pre_mask/z0",i)),
-  #        dir.create(paste0("./03_Outputs/05_Target_Rasters/v2023/pre_mask/z",i)))
-  # 
-  # # Save that directory path as a string
-  # ifelse(i<10, 
-  #        out_dir<- paste0("./03_Outputs/05_Target_Rasters/v2023/pre_mask/z0",i),
-  #        out_dir<- paste0("./03_Outputs/05_Target_Rasters/v2023/pre_mask/z",i))
-  
-  
-  # Crop, mask, and resample layers to each landfire zone---
-  
-  # Read in the step 1 forest mask from the LandFire EVC layer for this zone----
-  # if (i<10) {
-  #   evc<- rast(paste0("../03_Outputs/05_Target_Rasters/v2023/pre_mask/z0",i,"/evc.tif"))
-  # } else {
-  #   evc<- rast(paste0("../03_Outputs/05_Target_Rasters/v2023/pre_mask/z",i,"/evc.tif"))
-  # }
   
   # load desired mask for cropping and masking
   zmask <- terra::rast(glue::glue('{target_dir_mask_z}/evt_gp.tif'))
@@ -139,15 +98,6 @@ for (i in lf_zone_nums){
     # First crop climate data by the reprojected extent ----
     clim_crop <- terra::crop(clim, zmask_extent_reproj, snap="out")
     
-    # prcp_crop<- terra::crop(prcp, evc_extent_reproj, snap="out")
-    # srad_crop<- terra::crop(srad, evc_extent_reproj, snap="out") 
-    # swe_crop<- terra::crop(swe, evc_extent_reproj, snap="out")
-    # tmax_crop<- terra::crop(tmax, evc_extent_reproj, snap="out")
-    # tmin_crop<- terra::crop(tmin, evc_extent_reproj, snap="out")
-    # vp_crop<- terra::crop(vp, evc_extent_reproj, snap="out")
-    # vpd_crop<- terra::crop(vpd, evc_extent_reproj, snap="out")
-    
-    
     # Then reproject, resample, and mask to the tree mask for the given landfire zone ----
     clim_zone <- terra::crop(terra::project(x = clim_crop, 
                                             y = zmask,
@@ -156,113 +106,21 @@ for (i in lf_zone_nums){
                                             res = res(zmask),
                                             threads = TRUE),
                              zmask, mask = TRUE, touches = TRUE)
-    
-    # prcp_zone<- terra::crop(terra::project(x = prcp_crop, 
-    #                                         y = evc,
-    #                                         method = "cubic",
-    #                                         align = TRUE,
-    #                                         res = res(evc),
-    #                                         threads = TRUE),
-    #                          evc, mask = TRUE, touches = TRUE)
-    # #
-    # srad_zone<- terra::crop(terra::project(x = srad_crop, 
-    #                                         y = evc,
-    #                                         method = "cubic",
-    #                                         align = TRUE,
-    #                                         res = res(evc),
-    #                                         threads = TRUE),
-    #                          evc, mask = TRUE, touches = TRUE)
-    # #
-    # swe_zone<- terra::crop(terra::project(x = swe_crop, 
-    #                                        y = evc,
-    #                                        method = "cubic",
-    #                                        align = TRUE,
-    #                                        res = res(evc),
-    #                                        threads = TRUE),
-    #                         evc, mask = TRUE, touches = TRUE)
-    # # correct negative SWE values
-    # swe_zone[swe_zone < 0]<- 0
-    # 
-    # #
-    # tmax_zone<- terra::crop(terra::project(x = tmax_crop, 
-    #                                         y = evc,
-    #                                         method = "cubic",
-    #                                         align = TRUE,
-    #                                         res = res(evc),
-    #                                         threads = TRUE),
-    #                          evc, mask = TRUE, touches = TRUE)
-    # #
-    # tmin_zone<- terra::crop(terra::project(x = tmin_crop, 
-    #                                         y = evc,
-    #                                         method = "cubic",
-    #                                         align = TRUE,
-    #                                         res = res(evc),
-    #                                         threads = TRUE),
-    #                          evc, mask = TRUE, touches = TRUE)
-    # #
-    # vp_zone<- terra::crop(terra::project(x = vp_crop, 
-    #                                       y = evc,
-    #                                       method = "cubic",
-    #                                       align = TRUE,
-    #                                       res = res(evc),
-    #                                       threads = TRUE),
-    #                        evc, mask = TRUE, touches = TRUE)
-    # #
-    # vpd_zone<- terra::crop(terra::project(x = vpd_crop, 
-    #                                        y = evc,
-    #                                        method = "cubic",
-    #                                        align = TRUE,
-    #                                        res = res(evc),
-    #                                        threads = TRUE),
-    #                         evc, mask = TRUE, touches = TRUE)
-    
-    
-    # Save the final raster for each zone ----
-    
-    # writeRaster(clim_zone, 
-    #             glue::glue('{target_dir_mask_z}/{var}.tif'), 
-    #             datatype = "FLT4S", overwrite = TRUE)
-    
-    # writeRaster(prcp_zone, paste0(out_dir, "/prcp_normal_1981to2020.tif"), datatype = "FLT4S", overwrite = TRUE)
-    # writeRaster(srad_zone, paste0(out_dir, "/srad_normal_1981to2020.tif"), datatype = "FLT4S", overwrite = TRUE)
-    # writeRaster(swe_zone, paste0(out_dir, "/swe_normal_1981to2020.tif"), datatype = "FLT4S", overwrite = TRUE)
-    # writeRaster(tmax_zone, paste0(out_dir, "/tmax_normal_1981to2020.tif"), datatype = "FLT4S", overwrite = TRUE)
-    # writeRaster(tmin_zone, paste0(out_dir, "/tmin_normal_1981to2020.tif"), datatype = "FLT4S", overwrite = TRUE)
-    # writeRaster(vp_zone, paste0(out_dir, "/vp_normal_1981to2020.tif"), datatype = "FLT4S", overwrite = TRUE)
-    # writeRaster(vpd_zone, paste0(out_dir, "/vpd_normal_1981to2020.tif"), datatype = "FLT4S", overwrite = TRUE)
   
-    # Load final post-mask evc for the zone
-    #evc<- terra::rast(glue::glue('{target_dir_mask_z}/evc.tif'))
-    
-    # Load prcp and srad
-    #prcp<- terra::rast(paste0(in_dir,"/prcp.tif"))
-    #srad<- terra::rast(paste0(in_dir,"/srad.tif"))
     
     
     #Check valid pixel length
     #------------------------------------------------------------------------------------#
     
-    # Check that the number of valid pixels between EVC and Daymet are the same
-    # Only doing SRAD and PRCP, because Srad could be different but prcp and all others should be the same
-    #evc_valid_pixel_length<- length(values(evc, na.rm=T))
-    #prcp_valid_pixel_length<- length(values(prcp, na.rm=T))
-    #srad_valid_pixel_length<- length(values(srad, na.rm=T))
+    # Check that the number of valid pixels between Mask and Daymet are the same
   
     zmask_px <- length(values(zmask, na.rm = T))
     clim_px <- length(values(clim_zone, na.rm = T))
     
     print(glue::glue("zmask pixels: {zmask_px}, clim pixels: {clim_px} "))
     
-    # if (#evc_valid_pixel_length != srad_valid_pixel_length |
-    #     #evc_valid_pixel_length != prcp_valid_pixel_length
-    #     evc_valid_pixel_length != clim_valid_pixel_length) {
-    #   print(paste0("!! Mask layer and Daymet valid pixel length not identical for zone ",i,"!!"))
-    # } else {
-    #   print(paste0("Mask layer and Daymet valid pixel length are identical for zone ",i))
-    # }
     
-    
-    
+    # Either Export layer, or apply focal zone to make the reprojection align 
     if(zmask_px == clim_px) {
       
       print(glue::glue("Mask layer and DayMet have the same number of pixels! Exporting {var} for zone {i}"))
