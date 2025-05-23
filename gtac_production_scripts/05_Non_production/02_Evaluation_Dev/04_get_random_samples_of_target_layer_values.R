beaf2b2f9425051e17fe9c67c22cde3362e6942a
Lila Leatherman <31491157+lleather@users.noreply.github.com>
2025-05-23 10:02:04 -0600

    update target layer check scripts to use variables for files

--- gtac_production_scripts/05_Non_production/02_Evaluation_Dev/04_get_random_samples_of_target_layer_values.R
+++ gtac_production_scripts/05_Non_production/02_Evaluation_Dev/04_get_random_samples_of_target_layer_values.R
@@ -1,29 +1,66 @@
+# Evaluate target layer values against X table values
+# Get random samples from target layers to compare against values in X table
+
+# Written by Scott Zimmer
+# Updated by Lila Leatherman
+
+###################################################################################
+
+
 library(tidyverse)
 library(magrittr)
 library(terra)
 
 setwd("//166.2.126.25/Treemap")
 
+home_dir <- "//166.2.126.25/Treemap"
 
+year_in <- 2023
+target_vars <- c("disturb_code",
+                 "disturb_year",
+                 "easting",
+                 "elevation",
+                 "evc",
+                 "evh",
+                 "northing",
+                 "prcp",
+                 "slope",
+                 "srad",
+                 "swe",
+                 "tmax",
+                 "tmin",
+                 "vp",
+                 "vpd")
 
+#######################################################################
 
+target_dir <- glue::glue("{home_dir}/03_Outputs/05_Target_Rasters/v2023/")
+target_dir_mask <- glue::glue("{home_dir}/03_Outputs/05_Target_Rasters/v2023/one_mask/")
+
+target_dir_eval <- glue::glue('{target_dir}/eval/')
+target_dir_layer_comparison <- glue::glue('{target_dir}/eval/Value_Comparison_X_table_Target_Layers/target_layer_vectors/')
+
+if(!exists(target_dir_layer_comparison)) {
+  dir.create(target_dir_layer_comparison, recursive = TRUE)}
+
+  
 # Target Layer Values ----
 # direct to all target rasters
-dc_rasters<- list.files("./03_Outputs/05_Target_Rasters/v2023/one_mask/", pattern="disturb_code.tif$", full.names=T, recursive = T)
-dy_rasters<- list.files("./03_Outputs/05_Target_Rasters/v2023/one_mask/", pattern="disturb_year.tif$", full.names=T, recursive = T)
-easting_rasters<- list.files("./03_Outputs/05_Target_Rasters/v2023/one_mask/", pattern="easting.tif$", full.names=T, recursive = T)
-elevation_rasters<- list.files("./03_Outputs/05_Target_Rasters/v2023/one_mask/", pattern="elevation.tif$", full.names=T, recursive = T)
-evc_rasters<- list.files("./03_Outputs/05_Target_Rasters/v2023/one_mask/", pattern="evc.tif$", full.names=T, recursive = T)
-evh_rasters<- list.files("./03_Outputs/05_Target_Rasters/v2023/one_mask/", pattern="evh.tif$", full.names=T, recursive = T)
-northing_rasters<- list.files("./03_Outputs/05_Target_Rasters/v2023/one_mask/", pattern="northing.tif$", full.names=T, recursive = T)
-prcp_rasters<- list.files("./03_Outputs/05_Target_Rasters/v2023/one_mask/", pattern="prcp.tif$", full.names=T, recursive = T)
-slope_rasters<- list.files("./03_Outputs/05_Target_Rasters/v2023/one_mask/", pattern="slope.tif$", full.names=T, recursive = T)
-srad_rasters<- list.files("./03_Outputs/05_Target_Rasters/v2023/one_mask/", pattern="srad.tif$", full.names=T, recursive = T)
-swe_rasters<- list.files("./03_Outputs/05_Target_Rasters/v2023/one_mask/", pattern="swe.tif$", full.names=T, recursive = T)
-tmax_rasters<- list.files("./03_Outputs/05_Target_Rasters/v2023/one_mask/", pattern="tmax.tif$", full.names=T, recursive = T)
-tmin_rasters<- list.files("./03_Outputs/05_Target_Rasters/v2023/one_mask/", pattern="tmin.tif$", full.names=T, recursive = T)
-vp_rasters<- list.files("./03_Outputs/05_Target_Rasters/v2023/one_mask/", pattern="vp.tif$", full.names=T, recursive = T)
-vpd_rasters<- list.files("./03_Outputs/05_Target_Rasters/v2023/one_mask/", pattern="vpd.tif$", full.names=T, recursive = T)
+dc_rasters<- list.files(target_dir_mask, pattern="disturb_code_LF.tif$", full.names=T, recursive = T)
+dy_rasters<- list.files(target_dir_mask, pattern="disturb_year_LF.tif$", full.names=T, recursive = T)
+easting_rasters<- list.files(target_dir_mask, pattern="easting.tif$", full.names=T, recursive = T)
+elevation_rasters<- list.files(target_dir_mask, pattern="elevation.tif$", full.names=T, recursive = T)
+evc_rasters<- list.files(target_dir_mask, pattern="evc.tif$", full.names=T, recursive = T)
+evh_rasters<- list.files(target_dir_mask, pattern="evh.tif$", full.names=T, recursive = T)
+northing_rasters<- list.files(target_dir_mask, pattern="northing.tif$", full.names=T, recursive = T)
+prcp_rasters<- list.files(target_dir_mask, pattern="prcp.tif$", full.names=T, recursive = T)
+slope_rasters<- list.files(target_dir_mask, pattern="slope.tif$", full.names=T, recursive = T)
+srad_rasters<- list.files(target_dir_mask, pattern="srad.tif$", full.names=T, recursive = T)
+swe_rasters<- list.files(target_dir_mask, pattern="swe.tif$", full.names=T, recursive = T)
+tmax_rasters<- list.files(target_dir_mask, pattern="tmax.tif$", full.names=T, recursive = T)
+tmin_rasters<- list.files(target_dir_mask, pattern="tmin.tif$", full.names=T, recursive = T)
+vp_rasters<- list.files(target_dir_mask, pattern="vp.tif$", full.names=T, recursive = T)
+vpd_rasters<- list.files(target_dir_mask, pattern="vpd.tif$", full.names=T, recursive = T)
 
 
 # Load the landfire zones 
@@ -34,8 +71,13 @@ lf_zone_nums<- sort(lf_zones$ZONE_NUM)
 
 # Loop through all target layers, 
 for(i in lf_zone_nums){
+  #i = 1
+  
   # Create directory for outputs
-  dir.create(paste0("./03_Outputs/09_Value_Comparison_X_table_Target_Layers/v2023/z",i))
+  if(!exists(paste0(target_dir_layer_comparison, "z",i))){
+    dir.create(paste0(target_dir_layer_comparison, "z",i))
+  }
+  
   #Load raster
   r<- rast(dc_rasters[i])
   # Get non-NA values
@@ -45,7 +87,7 @@ for(i in lf_zone_nums){
   # Keep random sample of values
   vals_sub<- vals[random_ids]
   # Save it
-  saveRDS(vals_sub, paste0("./03_Outputs/09_Value_Comparison_X_table_Target_Layers/v2023/z",i,"/dc.rds"))
+  saveRDS(vals_sub, paste0(target_dir_layer_comparison, "z",i,"/dc.rds"))
   ########################
   #Load raster
   r<- rast(dy_rasters[i])
@@ -54,7 +96,7 @@ for(i in lf_zone_nums){
   # Keep random sample of values
   vals_sub<- vals[random_ids]
   # Save it
-  saveRDS(vals_sub, paste0("./03_Outputs/09_Value_Comparison_X_table_Target_Layers/v2023/z",i,"/dy.rds"))
+  saveRDS(vals_sub, paste0(target_dir_layer_comparison, "z",i,"/dy.rds"))
   ########################
   #Load raster
   r<- rast(easting_rasters[i])
@@ -63,7 +105,7 @@ for(i in lf_zone_nums){
   # Keep random sample of values
   vals_sub<- vals[random_ids]
   # Save it
-  saveRDS(vals_sub, paste0("./03_Outputs/09_Value_Comparison_X_table_Target_Layers/v2023/z",i,"/easting.rds"))
+  saveRDS(vals_sub, paste0(target_dir_layer_comparison, "z",i,"/easting.rds"))
   ########################
   #Load raster
   r<- rast(elevation_rasters[i])
@@ -72,7 +114,7 @@ for(i in lf_zone_nums){
   # Keep random sample of values
   vals_sub<- vals[random_ids]
   # Save it
-  saveRDS(vals_sub, paste0("./03_Outputs/09_Value_Comparison_X_table_Target_Layers/v2023/z",i,"/elevation.rds"))
+  saveRDS(vals_sub, paste0(target_dir_layer_comparison, "z",i,"/elevation.rds"))
   ########################
   #Load raster
   r<- rast(evc_rasters[i])
@@ -81,7 +123,7 @@ for(i in lf_zone_nums){
   # Keep random sample of values
   vals_sub<- vals[random_ids]
   # Save it
-  saveRDS(vals_sub, paste0("./03_Outputs/09_Value_Comparison_X_table_Target_Layers/v2023/z",i,"/evc.rds"))
+  saveRDS(vals_sub, paste0(target_dir_layer_comparison, "z",i,"/evc.rds"))
   ########################
   #Load raster
   r<- rast(evh_rasters[i])
@@ -90,7 +132,7 @@ for(i in lf_zone_nums){
   # Keep random sample of values
   vals_sub<- vals[random_ids]
   # Save it
-  saveRDS(vals_sub, paste0("./03_Outputs/09_Value_Comparison_X_table_Target_Layers/v2023/z",i,"/evh.rds"))
+  saveRDS(vals_sub, paste0(target_dir_layer_comparison, "z",i,"/evh.rds"))
   ########################
   #Load raster
   r<- rast(northing_rasters[i])
@@ -99,7 +141,7 @@ for(i in lf_zone_nums){
   # Keep random sample of values
   vals_sub<- vals[random_ids]
   # Save it
-  saveRDS(vals_sub, paste0("./03_Outputs/09_Value_Comparison_X_table_Target_Layers/v2023/z",i,"/northing.rds"))
+  saveRDS(vals_sub, paste0(target_dir_layer_comparison, "z",i,"/northing.rds"))
   ########################
   #Load raster
   r<- rast(prcp_rasters[i])
@@ -108,7 +150,7 @@ for(i in lf_zone_nums){
   # Keep random sample of values
   vals_sub<- vals[random_ids]
   # Save it
-  saveRDS(vals_sub, paste0("./03_Outputs/09_Value_Comparison_X_table_Target_Layers/v2023/z",i,"/prcp.rds"))
+  saveRDS(vals_sub, paste0(target_dir_layer_comparison, "z",i,"/prcp.rds"))
   ########################
   #Load raster
   r<- rast(slope_rasters[i])
@@ -117,7 +159,7 @@ for(i in lf_zone_nums){
   # Keep random sample of values
   vals_sub<- vals[random_ids]
   # Save it
-  saveRDS(vals_sub, paste0("./03_Outputs/09_Value_Comparison_X_table_Target_Layers/v2023/z",i,"/slope.rds"))
+  saveRDS(vals_sub, paste0(target_dir_layer_comparison, "z",i,"/slope.rds"))
   ########################
   #Load raster
   r<- rast(srad_rasters[i])
@@ -126,7 +168,7 @@ for(i in lf_zone_nums){
   # Keep random sample of values
   vals_sub<- vals[random_ids]
   # Save it
-  saveRDS(vals_sub, paste0("./03_Outputs/09_Value_Comparison_X_table_Target_Layers/v2023/z",i,"/srad.rds"))
+  saveRDS(vals_sub, paste0(target_dir_layer_comparison, "z",i,"/srad.rds"))
   ########################
   #Load raster
   r<- rast(swe_rasters[i])
@@ -135,7 +177,7 @@ for(i in lf_zone_nums){
   # Keep random sample of values
   vals_sub<- vals[random_ids]
   # Save it
-  saveRDS(vals_sub, paste0("./03_Outputs/09_Value_Comparison_X_table_Target_Layers/v2023/z",i,"/swe.rds"))
+  saveRDS(vals_sub, paste0(target_dir_layer_comparison, "z",i,"/swe.rds"))
   ########################
   #Load raster
   r<- rast(tmax_rasters[i])
@@ -144,7 +186,7 @@ for(i in lf_zone_nums){
   # Keep random sample of values
   vals_sub<- vals[random_ids]
   # Save it
-  saveRDS(vals_sub, paste0("./03_Outputs/09_Value_Comparison_X_table_Target_Layers/v2023/z",i,"/tmax.rds"))
+  saveRDS(vals_sub, paste0(target_dir_layer_comparison, "z",i,"/tmax.rds"))
   ########################
   #Load raster
   r<- rast(tmin_rasters[i])
@@ -153,7 +195,7 @@ for(i in lf_zone_nums){
   # Keep random sample of values
   vals_sub<- vals[random_ids]
   # Save it
-  saveRDS(vals_sub, paste0("./03_Outputs/09_Value_Comparison_X_table_Target_Layers/v2023/z",i,"/tmin.rds"))
+  saveRDS(vals_sub, paste0(target_dir_layer_comparison, "z",i,"/tmin.rds"))
   ########################
   #Load raster
   r<- rast(vp_rasters[i])
@@ -162,7 +204,7 @@ for(i in lf_zone_nums){
   # Keep random sample of values
   vals_sub<- vals[random_ids]
   # Save it
-  saveRDS(vals_sub, paste0("./03_Outputs/09_Value_Comparison_X_table_Target_Layers/v2023/z",i,"/vp.rds"))
+  saveRDS(vals_sub, paste0(target_dir_layer_comparison, "z",i,"/vp.rds"))
   ########################
   #Load raster
   r<- rast(vpd_rasters[i])
@@ -171,7 +213,7 @@ for(i in lf_zone_nums){
   # Keep random sample of values
   vals_sub<- vals[random_ids]
   # Save it
-  saveRDS(vals_sub, paste0("./03_Outputs/09_Value_Comparison_X_table_Target_Layers/v2023/z",i,"/vpd.rds"))
+  saveRDS(vals_sub, paste0(target_dir_layer_comparison, "z",i,"/vpd.rds"))
   ########################
   gc()
   print(paste0("Finished getting values from zone ",lf_zone_nums[i]))
