2a998d8bce43b2292563c6397ddaba0403eca0ed
LILA S. LEATHERMAN <Lila.Leatherman@usda.gov>
2025-12-19 10:13:36 -0700

    update new FIA var calc

--- gtac_production_scripts/05_Non_production/05_Standalone_Var_Assembly/01_calc_new_FIA_vars.R
+++ gtac_production_scripts/05_Non_production/05_Standalone_Var_Assembly/01_calc_new_FIA_vars.R
@@ -1,6 +1,12 @@
 # Calculate attributes from FIA variables that were not included in the original TreeMap outputs
 
 # This script does the following: 
+# Load TreeMap Raster Attribute Table (RAT) with TM_ID and PLT_CN
+# Load FIA data for plots corresponding to TreeMap PLT_CN
+# Calculate sound volume variables 
+# Join calculated variables back to RAT
+# Export updated RAT with new variables
+# (Use updated RAT in subsequent processing steps to create rasters of new attributes)
 
 
 ######################################################
@@ -11,74 +17,73 @@
 #-----------------------------------------------------#
 
 # pick a TreeMap vintage to use for assembly 
-tm_version = "2022_Production"
-tm_project_name = "2022_Production_newXtable"
+tm_year = 2022
 
-# export location
-output_dir = "//afssxgtacnas311/ForestMAP/80_Workspace/40_TreeMap_Volume/Attribute_assembly/"
+# FIA years to include in calculations
+start_year = 1999
+end_year = tm_year
 
-# name for output products
-output_project_name = "TM_Volume_Calcs"
+# set input paths. all these are relative to home_dir set in setup_dirs.R
+input_paths = c(
+  # export location
+  export_dir = "02_Outputs/01_Attribute_calcs/",
+  rat_orig_path = "01_Inputs/01_TreeMap_Data/2022/TM2022_RAT_tmid.csv"
 
-# # list of desired output fields
-# output_fields = c("VOLCFSND_Live_perAcre",
-#                   "VOLCFSND_Live_perPixel")
+)
 
-# # FIA tables needed
-# fia_tables = c("TREE", "COND")
+overwrite_rat = TRUE
+
+#fia_path = "05_FIA_DataMart/SQLite/SQLite_FIADB_States/SQLite_FIADB_WA.db"
+fia_path = "05_FIA_DataMart/SQLite/SQLite_FIADB_ENTIRE/SQLite_FIADB_ENTIRE.db"
 
 # write sql query for initial data pull
 # make sure you get all FIA tables and fields necessary here
-sql_query_init = 'select TREE.PLT_CN, TREE.DIA, TREE.CN as "TREE_CN", TPA_UNADJ, STATUSCD, TREECLCD, STANDING_DEAD_CD, VOLCFSND, VOLCFNET, COND.INVYR, COND.COND_STATUS_CD
+# can also check syntax by writing as a .sql file
+sql_query_init = 'select TREE.PLT_CN, TREE.DIA, TREE.CN as "TREE_CN", TPA_UNADJ, STATUSCD, TREECLCD, STANDING_DEAD_CD, VOLCFSND, VOLCFNET, FORTYPCD, COND.INVYR, COND.COND_STATUS_CD
 from COND
 INNER JOIN TREE ON COND.PLT_CN = TREE.PLT_CN'
 
+# placeholders for variables to be filled in via glue_sql
 sql_query_where = 'WHERE COND.INVYR >= {start_year} AND COND.INVYR <= {end_year} AND TREE.PLT_CN in ({id_list*})'
 
-# list zones of interest. options = "all" or a list of specific zones
-#zones_desired = "all"
-zones_desired = c(1, 2, 3)
+# csv of forest types for species and groups, with codes
+forest_types_species_to_group = read.csv("data/forest_types_species_to_group.csv")
 
-fia_path = "01_Data/04_FIA/05_FIA_DataMart/SQLite/SQLite_FIADB_ENTIRE/SQLite_FIADB_ENTIRE.db"
-#fia_path = "01_Data/04_FIA/05_FIA_DataMart/SQLite/SQLite_FIADB_States/SQLite_FIADB_WA.db"
-
-# rat path 
-rat_path = "//166.2.126.25/TreeMap/03_Outputs/06_Reference_Data/v2022/03_Raster_attributes/TM2022_RAT_tmid.csv"
-
-
-start_year = 1999
-end_year = 2022
-
-# Load data
-#---------------------------------------------------------------------
+# Run set up 
+#-------------------------------------------------------#
 # Allow for sufficient digits to differentiate plot cn numbers
 options("scipen"=100, "digits"=8)
 
-library(RSQLite)
-library(DBI)
+library_list = c(
+"glue",
+"RSQLite",
+"DBI",
+"tidyverse",
+"magrittr"
+)
 
+# load libraries
+invisible(lapply(library_list, function(x) library(x, character.only = TRUE)))
+
+## load setup functions
 this_proj <- this.path::this.proj()
 this_dir <- this.path::this.dir()
+setup_path = glue::glue('{this_proj}/functions/setup_functions.R')
+source(setup_path)
 
-## load treemap library
-lib_path = glue::glue('{this_proj}/gtac_production_scripts/00_Library/treeMapLib.R')
-source(lib_path)
+# append home_dir to input paths
+input_paths = lapply(input_paths, function(x) glue::glue("{home_dir}{x}"))
 
-# Load project params
-#project_params_path = glue::glue('{this_proj}/gtac_production_scripts/03_Imputation/params/{tm_version}_imputation_inputs.RDS')
-
-
-#load(project_params_path)
-
-fia_path = glue::glue("{home_dir}{fia_path}")
-
+# append fia_dir to fia_path
+fia_path = glue::glue("{FIA_dir}{fia_path}")
 
 # Load and prep Raster Attribute Table
 #-----------------------------------------------------------------#
 
-rat <- load_RAT(rat_path, 
-        CN_column = "PLT_CN", 
-        ID_column = "TM_ID")
+rat <- load_RAT(input_paths[['rat_orig_path']],
+                CN_column = "PLT_CN",
+                ID_column = "TM_ID"
+)
 
 # get list of ids to limit sql query of database
 id_list = rat$CN
@@ -88,7 +93,7 @@ id_list = rat$CN
 
 # connect to FIA db
 db_path <- fia_path
-print(file.exists(db_path)) # Should return TRUE if the file exists
+#print(file.exists(db_path)) # Should return TRUE if the file exists
 
 con = dbConnect(RSQLite::SQLite(), db_path)
 
@@ -97,21 +102,24 @@ con = dbConnect(RSQLite::SQLite(), db_path)
 # alltables
 
 # # list fields in table 
-dbListFields(con, "PLOT")
-dbListFields(con, "TREE")
-dbListFields(con, "COND")
+# dbListFields(con, "PLOT")
+# dbListFields(con, "TREE")
+# dbListFields(con, "COND")
 
 
 sql_query = glue::glue('{sql_query_init} {sql_query_where}')
 
-query <- glue_sql(  sql_query,  .con = con )
+# write to query.sql file for inspection
+writeLines(sql_query, con = "query.sql")
 
-print("final sql query, before glue_sql: ")
+query <- glue::glue_sql(  sql_query,  .con = con )
+
+message("final sql query, before glue_sql: ")
 print(sql_query)
 
 
 # Execute the query, passing R variables in the 'params' argument
-print("running query....")
+message("running query (this may take a while on the national FIADB)....")
 result_df <- dbGetQuery(con, query) 
 
 result_df %<>%   
@@ -119,14 +127,17 @@ result_df %<>%
   dplyr::group_by(PLT_CN) %>%
   arrange(PLT_CN)
 
-print("result df looks like this:")
-print(head(result_df))
-print(str(result_df))
+# message("result df looks like this:")
+# print(head(result_df))
+# print(str(result_df))
+
+# disconnect from db
+dbDisconnect(con)
 
 # Calculate variables
 #------------------------------------------------------------------------------------#
 
-print("Calculating variables")
+message("Calculating variables...")
 
 # VOLCFNET_D: Volume, Standing Dead (ft3/acre)
 # Volume, standing dead, cubic feet per acre. Calculated via the following FIA query:
@@ -172,6 +183,7 @@ VOLCFSND_D <- result_df %>%
   distinct()
 
 
+
 # Join all vars together
 #--------------------------------------------#
 d_list <- list(VOLCFNET_L, VOLCFNET_D, VOLCFSND_L, VOLCFSND_D)
@@ -181,68 +193,91 @@ attributes_out <- purrr::reduce(d_list,
               by = "PLT_CN") %>%
   dplyr::rename("CN" = PLT_CN) %>%
   dplyr::mutate(CN = as.numeric(CN),
+                # if VOLCFNET_L = 0 and VOLCFSND_L > 0, set VOLCFSND_L to 0
+                VOLCFSND_L = ifelse(VOLCFNET_L == 0 & VOLCFSND_L > 0, 0, VOLCFSND_L),
+                VOLCFSND_D = ifelse(VOLCFNET_D == 0 & VOLCFSND_D > 0, 0, VOLCFSND_D),
+                # calculate pixel values
                 VOLCFSND_L_PX = VOLCFSND_L * 900 * 0.000247104393046628 , # sqm/pixel * acres/sqm
                 VOLCFSND_D_PX = VOLCFSND_D * 900 * 0.000247104393046628
 )
 
+#str(attributes_out)
+
+# check for duplicates
+message("checking for duplicate CNs in attributes dataset...")
 dupes <- attributes_out %>%
   group_by(CN) %>%
   filter(n() > 1)
 
-
-# check for duplicates
-print("duplicate CNs - first 6")
-print(head(dupes))
-
-print("head of attributes dataset:")
-print(head(attributes_out))
-
-# remove CNs with no info available 
+if(nrow(dupes) > 0) {
+  stop("duplicate CNs found in attributes dataset. Please investigate.")
+  } else {
+    message("... no duplicate CNs found.")
+  }
 
 
-# Join to existing RAT to get TMID
+# message("duplicate CNs - first 6")
+# print(head(dupes))
+
+# message("head of attributes dataset:")
+# print(head(attributes_out))
+
+# Finalize output RAT
+#--------------------------------------------#
+
+# Join to existing RAT to get TMID, FORTYPCD, any other vars of interest
 rat_out = rat %>%
   dplyr::select(-c(VOLCFNET_D, VOLCFNET_L)) %>% # remove these fields since i'm recalculating them 
   dplyr::right_join(attributes_out, by = "CN")  %>%
   replace(is.na(.), 0) %>%
   # Select variables of interest
-  dplyr::select(CN, TM_ID, VOLCFNET_L, VOLCFSND_L, VOLCFSND_D, VOLCFNET_D, VOLCFSND_D_PX, VOLCFSND_L_PX) %>%
+  dplyr::select(CN, TM_ID, FORTYPCD, VOLCFNET_L, VOLCFSND_L, VOLCFSND_D, VOLCFNET_D, VOLCFSND_D_PX, VOLCFSND_L_PX) %>%
   arrange(CN)
 
-print(head(rat_out))
-
-# exclude any records where VOLCFNET_L = 0 and VOLCFSND > 0
+# Determine forest type group code from species code
 rat_out %<>%
-  filter(!(VOLCFNET_L == 0 & VOLCFSND_L > 0))
+  left_join(forest_types_species_to_group,
+       by = c("FORTYPCD" = "Species_Code")) 
+
+# Export
+#----------------------------------------------------------------#
+
+rat_export_path = glue::glue("{input_paths[['export_dir']]}01_Raster_attribute_table/TreeMap{tm_year}_rat_volumeCalcs.csv")
 
 # write out to table
-write.csv(rat_out, glue::glue("{output_dir}00_Raster_attribute_table/TreeMap_RAT_SOUNDVOLUME.csv"))
+if(overwrite_rat | !file.exists(rat_export_path)) {
+  message("writing out updated RAT with new variables...")
+  write.csv(rat_out, rat_export_path, row.names = FALSE)
+  message(glue::glue("...RAT written to {rat_export_path}"))
+
+} else {
+  stop("output RAT file already exists and overwrite_rat is set to FALSE. Set overwrite_rat to TRUE to overwrite existing file.")
+}
+
 
 # inspect
 #----------------------------------------------------------------#
 
-hist(rat_out$VOLCFSND_L)
-hist(rat_out$VOLCFNET_L)
-hist(rat_out$VOLCFSND_D)
+# hist(rat_out$VOLCFSND_L)
+# hist(rat_out$VOLCFNET_L)
+# hist(rat_out$VOLCFSND_D)
 
-plot(rat_out$VOLCFNET_L, rat_out$VOLCFSND_L)
-plot(rat_out$VOLCFNET_L, rat_out$VOLCFSND_L, xlim = c(0, 10000), ylim = c(0, 10000))
-abline(a = 0, b = 1, col = "red", lty = 2, lwd = 2)
-plot(rat_out$VOLCFNET_D, rat_out$VOLCFSND_D)
-abline(a = 0, b = 1, col = "red", lty = 2, lwd = 2)
+# plot(rat_out$VOLCFNET_L, rat_out$VOLCFSND_L)
+# plot(rat_out$VOLCFNET_L, rat_out$VOLCFSND_L, xlim = c(0, 10000), ylim = c(0, 10000))
+# abline(a = 0, b = 1, col = "red", lty = 2, lwd = 2)
+# plot(rat_out$VOLCFNET_D, rat_out$VOLCFSND_D)
+# abline(a = 0, b = 1, col = "red", lty = 2, lwd = 2)
 
-plot(rat_out$VOLCFNET_D, rat_out$VOLCFSND_D)
-plot(rat_out$VOLCFSND_L, rat_out$VOLCFSND_D)
-plot(rat_out$VOLCFNET_L, rat_out$VOLCFNET_D)
-plot()
+# plot(rat_out$VOLCFNET_D, rat_out$VOLCFSND_D)
+# plot(rat_out$VOLCFSND_L, rat_out$VOLCFSND_D)
+# plot(rat_out$VOLCFNET_L, rat_out$VOLCFNET_D)
 
-attributes_export = c("VOLCFSND_L_PX", "VOLCFSND_D_PX")
 
-rat_out %>%
-  filter(VOLCFNET_L== 0) %>%
-  ggplot() +
-  geom_point(aes(VOLCFNET_L, VOLCFSND_L))
+# rat_out %>%
+#   filter(VOLCFNET_L== 0) %>%
+#   ggplot() +
+#   geom_point(aes(VOLCFNET_L, VOLCFSND_L))
 
-rat_out %>%
-  filter(VOLCFNET_L== 0,
-         VOLCFSND_L > 0)
+# rat_out %>%
+#   filter(VOLCFNET_L== 0,
+#          VOLCFSND_L > 0)
