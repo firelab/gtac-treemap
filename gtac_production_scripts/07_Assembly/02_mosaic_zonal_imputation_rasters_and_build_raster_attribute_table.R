ee3c4dd03eabdd85362fa69759c9acd96c3d0814
LILA S. LEATHERMAN <Lila.Leatherman@usda.gov>
2026-01-08 10:56:38 -0700

    update attribute tables with continuous FVS vars - quick fix

--- gtac_production_scripts/07_Assembly/02_mosaic_zonal_imputation_rasters_and_build_raster_attribute_table.R
+++ gtac_production_scripts/07_Assembly/02_mosaic_zonal_imputation_rasters_and_build_raster_attribute_table.R
@@ -26,7 +26,7 @@ mosaic_dir <- glue::glue("03_Outputs/07_Projects/{project_name}/04_Mosaic_assemb
 
 # paths to data - relative to home_dir
 dbf_table_path <- glue::glue("{mosaic_dir}/TreeMap{year}.tif.vat.dbf") 
-attribute_table_path <- glue::glue("03_Outputs/06_Reference_Data/v{year}/03_Raster_attributes/TM{year}_RAT_tmid.csv") 
+attribute_table_path <- glue::glue("03_Outputs/06_Reference_Data/v{year}/03_Raster_attributes/TM{year}_RAT_tmid_contValues.csv") 
 
 
 # Load TreeMap script library
@@ -60,6 +60,8 @@ if (!file.exists(mosaic_dir)){
 
 # Mosaic Imputation Rasters 
 #--------------------------------------------------#
+message("Mosaicking imputation rasters...")
+
 #Direct to all zonal imputation rasters
 imputation_rasters<- list.files(assembled_dir,
                                 pattern = "Production_Imputation.tif$", full.names = T, recursive = T)
@@ -71,6 +73,8 @@ terra::vrt(imputation_rasters, glue::glue("{mosaic_dir}/imputation_vrt.vrt"),
 
 # Load the VRT
 imputation <- rast(glue::glue("{mosaic_dir}/imputation_vrt.vrt"))
+
+message("Building raster attribute table...")
 
 # Build raster attribute table - calculate frequencies
 f<- terra::freq(imputation)[,c(2:3)]
@@ -93,6 +97,8 @@ out <- left_join(f, rat, by = c("Value" = "TM_ID")) %>%
 str(out)
 
 levels(imputation) <- out
+
+message("Exporting mosaicked raster...")
 
 if(file.exists(tif_out_path)) {
   message(glue::glue("Warning: overwriting existing tif: {tif_out_path}"))
