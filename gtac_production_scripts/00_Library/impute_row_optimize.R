c92f6201b315bc9e47ab8ea49f94da89e7f6e0bd
snzimmer <Scott.Zimmer@usda.gov>
2026-02-19 16:02:20 -0700

    Update impute_row_optimize.R

--- gtac_production_scripts/00_Library/impute_row_optimize.R
+++ gtac_production_scripts/00_Library/impute_row_optimize.R
@@ -13,8 +13,20 @@ impute_row_optimize <- function(dat, yai) {
   # Subset dataframe to valid data only
   X_temp <- dat[valid_cols, names(yai$xRefs), drop = FALSE]
   
+  # Set factor levels
+  #make sure they match input factor levels in reference data used in model
+  #-------------------------------------------------------#
+  X_temp <- 
+    X_temp %>%
+    dplyr::mutate(evt_gp_remap = factor(evt_gp_remap, levels = levels(yai$xRefs$evt_gp_remap)),
+                  disturb_code_bin = factor(disturb_code_bin, levels = levels(yai$xRefs$disturb_code_bin))) %>%
+    # put columns in order expected
+    dplyr::select(names(yai$xRefs))
+  
+  
   # Change row names so they don't match rownames of references used to build the model
   attr(X_temp, "row.names") <- as.integer(seq_len(nrow(X_temp)) + 1000000)
+  
   
   # Run imputation on the data
   temp_newtargs <- yaImpute::newtargets(yai, newdata = X_temp)
@@ -26,5 +38,3 @@ impute_row_optimize <- function(dat, yai) {
   #
   return(impute_out)
 }
-
-
